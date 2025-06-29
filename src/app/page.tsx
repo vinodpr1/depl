ddd463b903a159af42643871d32bfd3585f7b5a8
vinod <vinodpr737947@gmail.com>
2025-06-29 09:29:58 +0530

    added ls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 'use client'
-import React from 'react';
+import React, {useState} from 'react';
 
 const BirthdayWish = () => {
+
+  const [name, setName] = useState("Ambuj");
  
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-pink-100 to-purple-200 p-4">
      
       <div className="max-w-md w-full bg-white rounded-2xl shadow-xl overflow-hidden transform transition-all hover:scale-105 duration-300">
         <div className="bg-gradient-to-r from-pink-500 to-purple-600 p-6 text-center">
-          <h1 className="text-4xl font-bold text-white mb-2">Happy Birthday beta kk!</h1>
+          <h1 className="text-4xl font-bold text-white mb-2">Happy Birthday beta {name}!</h1>
           <p className="text-xl text-white opacity-90">🎉 bhai 🎂</p>
+          <input className='border px-2 py-2 rounded border-gray-500 text-gray-500' type="text" onChange={(e)=>setName(e.target.value)}/>
         </div>
+      
         
         <div className="p-6 text-center">
           <p className="text-gray-700 mb-4">
