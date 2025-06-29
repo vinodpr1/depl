b69d598638ea65bef478f9241c59009586f7cbb4
vinod <vinodpr737947@gmail.com>
2025-06-29 10:30:12 +0530

    CD pipeline fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const BirthdayWish = () => {
      
       <div className="max-w-md w-full bg-white rounded-2xl shadow-xl overflow-hidden transform transition-all hover:scale-105 duration-300">
         <div className="bg-gradient-to-r from-pink-500 to-purple-600 p-6 text-center">
-          <h1 className="text-4xl font-bold text-white mb-2">Happy Birthday beta {name}!</h1>
+          <h1 className="text-4xl font-bold text-white mb-2">Happy Birthday betaaaaaaaaaaaaaaaaaa {name}!</h1>
           <p className="text-xl text-white opacity-90">🎉 bhai 🎂</p>
           <input className='border px-2 py-2 rounded border-gray-500 text-gray-500' type="text" onChange={(e)=>setName(e.target.value)}/>
         </div>
